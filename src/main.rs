43105cc1a6e2c3ddce75446cd6e1c03b07ffd667
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-10-11 08:12:14 -0300

    handling GET, POST, PUT

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
-use std::thread;
-use std::time::Duration;
 
 const BUFFER_LEN: usize = 512;
+
+#[derive(PartialEq)]
+enum RequestType {
+    GET,
+    POST,
+    PUT,
+}
+
+// Function to handle GET, POST, and PUT requests
+fn handle_request(request: &str) -> Result<(RequestType, String), String> {
+    let mut lines = request.lines();
+    if let Some(request_line) = lines.next() {
+        // The request line looks like: "GET / HTTP/1.1"
+        let parts: Vec<&str> = request_line.split_whitespace().collect();
+        if parts.len() >= 3 {
+            let method = parts[0];
+            let path = parts[1];
+
+            match method {
+                "GET" => Ok((RequestType::GET, path.into())),
+                "POST" => Ok((RequestType::POST, path.into())),
+                "PUT" => Ok((RequestType::PUT, path.into())),
+                _ => Err("Unsupported method".to_string()),
+            }
+        } else {
+            eprintln!("Bad Request: {request_line}");
+            Err("Invalid Request format".to_string())
+        }
+    } else {
+        eprintln!("hr: Empty request");
+        Err("Empty Request".to_string())
+    }
+}
 
 fn main() -> std::io::Result<()> {
     // Bind the server to localhost:7878
@@ -17,20 +48,55 @@ fn main() -> std::io::Result<()> {
             Ok(mut stream) => {
                 println!("New connection: {}", stream.peer_addr()?);
 
-                println!("Taking a nap");
-                thread::sleep(Duration::from_secs(2));
-
                 // Create a buffer to store incoming data
                 let mut buffer = [0; BUFFER_LEN];
                 let mut total_bytes_received = 0;
 
+                // handle request type
+                match stream.read(&mut buffer) {
+                    Ok(bytes_read) => {
+                        if bytes_read == 0 {
+                            eprintln!("Empty request?!");
+                            continue;
+                        }
+                        // Convert buffer to string to get the request
+                        let request = String::from_utf8_lossy(&buffer[..]);
+
+                        match handle_request(&request) {
+                            Ok(req) => {
+                                println!("Path: {}", req.1);
+                                if req.0 == RequestType::GET {
+                                    let response = get_200_response("OK");
+                                    if let Err(e) = stream.write_all(response.as_bytes()) {
+                                        eprintln!("Failed wrting to stream: {e}");
+                                    }
+                                    stream.flush()?;
+                                    continue;
+                                }
+                            },
+                            Err(e) => {
+                                let response = get_400_response(&e);
+                                if let Err(e) = stream.write_all(response.as_bytes()) {
+                                    eprintln!("Failed wrting to stream: {e}");
+                                }
+                                stream.flush()?;
+                                continue;
+                            },
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("rt: Failed reading stream: {e}");
+                        continue;
+                    }
+                }
+
+                // Reading body
                 loop {
                     match stream.read(&mut buffer) {
                         Ok(bytes_read) => {
                             if bytes_read == 0 {
                                 break;
                             }
-                            println!("\n<<Received {bytes_read}>>");
                             total_bytes_received += bytes_read;
 
                             // Echo the received message back to the client
@@ -68,4 +134,17 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn get_200_response(arg: &str) -> String {
+    let len = arg.len();
+    format!("HTTP/1.1 200 OK\r\n\
+                                Content-Type: text/html; charset=UTF-8\r\n\
+                                Content-Length: {len}\r\n\
+                                Connection: close\r\n\r\n\
+                                {arg}")
+}
+
+fn get_400_response(error_message: &str) -> String {
+    format!("HTTP/1.1 400 Bad Request\r\n\r\n{}", error_message)
 }
