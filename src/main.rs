c9528a8f120d1dcc0db238df6e44383c01d192ed
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-10-11 07:30:06 -0300

    http 1.1 response

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpListener;
 use std::thread;
 use std::time::Duration;
 
+const BUFFER_LEN: usize = 512;
+
 fn main() -> std::io::Result<()> {
     // Bind the server to localhost:7878
     let listener = TcpListener::bind("127.0.0.1:7878")?;
@@ -16,10 +18,10 @@ fn main() -> std::io::Result<()> {
                 println!("New connection: {}", stream.peer_addr()?);
 
                 println!("Taking a nap");
-                thread::sleep(Duration::from_secs(3));
+                thread::sleep(Duration::from_secs(2));
 
                 // Create a buffer to store incoming data
-                let mut buffer = [0; 512];
+                let mut buffer = [0; BUFFER_LEN];
                 let mut total_bytes_received = 0;
 
                 loop {
@@ -28,13 +30,14 @@ fn main() -> std::io::Result<()> {
                             if bytes_read == 0 {
                                 break;
                             }
+                            println!("\n<<Received {bytes_read}>>");
                             total_bytes_received += bytes_read;
 
                             // Echo the received message back to the client
                             print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
-                            // stream.write_all(&buffer[..bytes_read])?;
-                            if let Err(e) = stream.write_all(b"Hi") {
-                                eprintln!("Failed wrting to stream: {e}");
+
+                            if bytes_read < BUFFER_LEN {
+                                break;
                             }
                         }
                         Err(e) => {
@@ -44,6 +47,19 @@ fn main() -> std::io::Result<()> {
                     }
                 }
                 println!("Received total {total_bytes_received} bytes.\n");
+
+                // Construct an HTTP/1.1 response
+                let response = "HTTP/1.1 200 OK\r\n\
+                                Content-Type: text/html; charset=UTF-8\r\n\
+                                Content-Length: 13\r\n\
+                                Connection: close\r\n\r\n\
+                                Hello, world!";
+
+                // Send the response back to the client
+                if let Err(e) = stream.write_all(response.as_bytes()) {
+                    eprintln!("Failed wrting to stream: {e}");
+                }
+                stream.flush()?;
             }
             Err(e) => {
                 eprintln!("Connection failed: {}", e);
