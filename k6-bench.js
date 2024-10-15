a8b189d1b03792e0bbc03c51c57adc4cce0cd694
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-10-15 08:47:25 -0300

    improve k6 benchmark script
         checks.........................: 100.00% ✓ 84402       ✗ 0
         data_received..................: 8.6 MB  359 kB/s
         data_sent......................: 7.2 MB  299 kB/s
         group_duration.................: avg=1.07ms   min=388.8µs  med=757.07µs max=205.52ms p(90)=1.4ms    p(95)=1.68ms
         http_req_blocked...............: avg=296.15µs min=148.69µs med=248µs    max=16.76ms  p(90)=426.09µs p(95)=555.92µs
         http_req_connecting............: avg=204.6µs  min=96.79µs  med=166.22µs max=3.84ms   p(90)=302.82µs p(95)=412.68µs
         http_req_duration..............: avg=685.71µs min=132.69µs med=364.35µs max=205.11ms p(90)=948.63µs p(95)=1.18ms
           { expected_response:true }...: avg=685.71µs min=132.69µs med=364.35µs max=205.11ms p(90)=948.63µs p(95)=1.18ms
         http_req_failed................: 0.00%   ✓ 0           ✗ 84402
         http_req_receiving.............: avg=84.13µs  min=22.83µs  med=71.86µs  max=8.08ms   p(90)=108.67µs p(95)=143.73µs
         http_req_sending...............: avg=54.73µs  min=15.71µs  med=44.34µs  max=9.74ms   p(90)=71.02µs  p(95)=97.91µs
         http_req_tls_handshaking.......: avg=0s       min=0s       med=0s       max=0s       p(90)=0s       p(95)=0s
         http_req_waiting...............: avg=546.84µs min=63.69µs  med=230.96µs max=204.91ms p(90)=780.32µs p(95)=999.69µs
         http_reqs......................: 84402   3516.663579/s
         iteration_duration.............: avg=1.1ms    min=417.43µs med=788.15µs max=205.55ms p(90)=1.44ms   p(95)=1.72ms
         iterations.....................: 84402   3516.663579/s
         vus............................: 1       min=1         max=7
         vus_max........................: 8       min=8         max=8
    
    running (24.0s), 0/8 VUs, 84402 complete and 0 interrupted iterations

--- k6-bench.js
+++ k6-bench.js
@@ -4,9 +4,9 @@ import { check, group } from 'k6';
 
 export let options = {
    stages: [
-       { duration: '0.5m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
-       { duration: '0.5m', target: 4}, // stay at 4 virtual users for 0.5 minutes
-       { duration: '0.5m', target: 0 }, // ramp-down to 0 users
+       { duration: '0.1m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
+       { duration: '0.2m', target: 8}, // stay at 4 virtual users for 0.5 minutes
+       { duration: '0.1m', target: 0 }, // ramp-down to 0 users
      ]
 };
 
